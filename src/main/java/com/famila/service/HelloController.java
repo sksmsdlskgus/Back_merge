20e5d4cb0e5115c09d044534fa3348af0c3c845e
leyn <limleyn53@gmail.com>
2023-09-20 19:17:56 +0900

    dfdg

---
+++ src/main/java/com/famila/service/HelloController.java
@@ -0,0 +1,22 @@
+package com.famila.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Hello World!!");
+        return "hello";
+    }
+
+}
+
+
+
+
